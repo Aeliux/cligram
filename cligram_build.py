252867038686ed3728f40d0150328cae7711e87b
Alireza Poodineh <itsaeliux@gmail.com>
2025-11-20 21:12:59 +0330

    Enable optimizations for native binaries
    Signed-off-by: Alireza Poodineh <itsaeliux@gmail.com>

--- cligram_build.py
+++ cligram_build.py
@@ -3,6 +3,7 @@
 # Retrieved 2025-11-20, License - CC BY-SA 4.0
 
 import shutil
+import sys
 from distutils.command.build_ext import build_ext
 from distutils.errors import CCompilerError, DistutilsExecError, DistutilsPlatformError
 from pathlib import Path
@@ -11,14 +12,29 @@ from setuptools import Distribution, Extension, setup
 
 
 def build():
+    # Determine platform-specific optimization flags
+    if sys.platform == "win32":
+        extra_compile_args = ["/O2", "/GL", "/favor:blend"]
+        extra_link_args = ["/LTCG"]
+    else:
+        # Unix-like systems (Linux, macOS, etc.)
+        extra_compile_args = [
+            "-O3",
+            "-march=native",
+            "-ffast-math",
+            "-funroll-loops",
+            "-finline-functions",
+            "-DNDEBUG",
+        ]
+        extra_link_args = []
+
     ext_modules = [
         Extension(
             name="cligram.utils._device_native",
             sources=["src/cligram/utils/_device_native.c"],
             include_dirs=[],
-            define_macros=[],
-            extra_compile_args=[],
-            extra_link_args=[],
+            extra_compile_args=extra_compile_args,
+            extra_link_args=extra_link_args,
         )
     ]
 
